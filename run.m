29f2c4379020363011fd095b49d6aac3735347a9
Julian Brackins <julian.brackins@mines.sdsmt.edu>
2015-11-12 20:54:27 -0700

    so fricken done

--- run.m
+++ run.m
@@ -1,13 +1,13 @@
 %Files Names
-File1 = 'img\Yosemite\Yosemite1.jpg';;
+File1 = 'img\Yosemite\Yosemite1.jpg';
 File2 = 'img\Yosemite\Yosemite2.jpg';
 
 %Feature Thresholds. Higher Thresholds restrict what features will display
-Threshold1 = 1000;
-Threshold2 = 1000;
+Threshold1 = 25;
+Threshold2 = 25;
 
 %SSD Tolerance. Lower values restrict what distances are accepted
-SSDTolerance = .85;
+SSDTolerance = .55;
 
 %This runs the program. Please adjust commented portions in GetFeatures()
 %if you would like to run other Feature Descriptor methods 
@@ -16,3 +16,14 @@ FeatureDetection( File1,      ...
                   Threshold1, ...
                   Threshold2, ...
                   SSDTolerance   );
+              
+              
+              
+%REMINDERS
+%imcrop
+%imrotate
+%v1 = reshape(patch,[],1) 
+%Histogram equalize the image
+%histeq()
+%ImGradAngle = atan2(Iy,Ix) * 180 / pi;
+%quiver gives grid of xy points
